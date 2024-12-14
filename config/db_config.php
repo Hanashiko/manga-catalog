59936b469e62df01f26d0d7908d96714c1bd6dc1
Hanashiko <hlichisper@gmail.com>
2024-12-14 20:38:34 +0200

    Added readme

--- config/db_config.php
+++ config/db_config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'php');  
-define('DB_PASSWORD', '');      
+define('DB_PASSWORD', 'mAp101HG');      
 define('DB_DATABASE', 'manga_catalog');
 
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
